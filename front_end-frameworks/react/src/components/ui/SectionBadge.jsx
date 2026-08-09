90465cfbbb67b3bd8919dbafd457b007468c8bdd
thomas rousseau <thomas.rousseau19@gmail.com>
2026-08-09 10:56:39 +0200

    correction npm run fix

--- front_end-frameworks/react/src/components/ui/SectionBadge.jsx
+++ front_end-frameworks/react/src/components/ui/SectionBadge.jsx
@@ -2,7 +2,7 @@ import { Astroid } from "lucide-react";
 
 function SectionBadge({ text, className = "" }) {
     return (
-        <p className="px-4 py-2 text-xs text-violet-300 rounded-full border border-violet-500/20 bg-violet-500/10 inline-flex items-center gap-1 ">
+        <p className={`px-4 py-2 text-xs text-violet-300 rounded-full border border-violet-500/20 bg-violet-500/10 inline-flex items-center gap-1 ${className}`}>
             <Astroid color="#C4B5FD" fill="#C4B5FD" size={4} />
                 {text}
             <Astroid color="#C4B5FD" fill="#C4B5FD" size={4} />
