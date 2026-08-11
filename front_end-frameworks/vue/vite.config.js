25a628b6579886c221d0e2a0bd2c32b48ef9a72f
thomas rousseau <thomas.rousseau19@gmail.com>
2026-08-11 11:05:11 +0200

    creation architecture

--- front_end-frameworks/vue/vite.config.js
+++ front_end-frameworks/vue/vite.config.js
@@ -3,8 +3,13 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    vue(),
-  ],
+    base: "./",
+    plugins: [
+        vue(),
+        tailwindcss(),
+    ],
+    server: {
+        host: "0.0.0.0",
+        port: 3000,
+    },
 });
