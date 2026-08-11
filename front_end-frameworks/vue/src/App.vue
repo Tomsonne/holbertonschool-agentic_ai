a4aee02139b750a4db99d76c0c2eba687b12276a
thomas rousseau <thomas.rousseau19@gmail.com>
2026-08-11 11:11:49 +0200

    creation architecture

--- front_end-frameworks/vue/src/App.vue
+++ front_end-frameworks/vue/src/App.vue
@@ -1,6 +1,23 @@
 <script setup>
+import Header from "./components/layout/Header.vue";
+import Footer from "./components/layout/Footer.vue";
+import Hero from "./sections/Hero.vue";
+import About from "./sections/About.vue";
+import Features from "./sections/Features.vue";
+import Insights from "./sections/Insights.vue";
+import Contact from "./sections/Contact.vue";
 </script>
 
 <template>
-  <HelloWorld />
+    <Header />
+
+    <main>
+        <Hero />
+        <About />
+        <Features />
+        <Insights />
+        <Contact />
+    </main>
+
+    <Footer />
 </template>
